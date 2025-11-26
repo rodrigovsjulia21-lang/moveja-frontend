7ac4ae656600b197f0024007416c8bd78acd8670
rodrigovsjulia21-lang <rodrigovsjulia21@gmail.com>
2025-11-26 04:35:52 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: false,
+  experimental: { appDir: true },
+  env: {
+    NEXT_PUBLIC_API: process.env.NEXT_PUBLIC_API,
+  },
 };
 
 module.exports = nextConfig;
